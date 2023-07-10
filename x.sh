343e8d19c5a84415df3c823f1f3ebd86920b0015
xiaojian xu <138852548+xxj0303@users.noreply.github.com>
2023-07-10 09:55:31 +0800

    Update x.sh
    adu

--- x.sh
+++ x.sh
@@ -1,2 +1,2 @@
 export AMPLXE_EXPERIMENTAL=1
-/opt/intel/vtune_amplifier_xe/bin64/amplxe-cl -collect hotspots /opt/taobao/install/jdk-1.7.0_10/bin/java -classpath target/classes/:target/test-classes/ com.alibaba.json.test.benchmark.BenchmarkMain
+/opt/intel/vtune_amplifier_xe/bin64/amplxe-cl-adu -collect hotspots /opt/taobao/install/jdk-1.7.0_10/bin/java -classpath target/classes/:target/test-classes/ com.alibaba.json.test.benchmark.BenchmarkMain
